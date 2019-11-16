abdb3866925dcefe5f1f98844eeb22169c2d8e21
Дмитрий Костяков <miniBIGboss2@gmail.com>
2019-11-16 13:18:57 +0300

    Task 2 added

--- ProjectEuler/2.py
+++ ProjectEuler/2.py
@@ -2,3 +2,4 @@
 the first 10 terms will be: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ... By considering the terms in the
 Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."""
 
+# TODO написать генератор чисел Фибоначчи. Функция yield 
