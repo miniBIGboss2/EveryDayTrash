76ad88114f390b83b2affa2403c42abe9de47937
Дмитрий Костяков <miniBIGboss2@gmail.com>
2019-11-27 14:04:37 +0300

    new staff

--- ProjectWebScraping/TestLilianaOfTheVeil.py
+++ ProjectWebScraping/TestLilianaOfTheVeil.py
@@ -16,5 +16,13 @@ def get_card_name():
     print(card_name.get_text())
 
 
+def get_card_image():
+    html = urlopen("https://starcitygames.com/liliana-of-the-veil-sgl-mtg-isd-105-enn/")
+    bs_obj = BeautifulSoup(html, features="html.parser")
+    card_image = bs_obj.find("data-image-gallery-main data-zoom-image", {"class": "https://cdn11.bigcommerce.com/s-3b5vpig99v/images/stencil/1280x1280/products/204094/140256/LilianaOfTheVeil__29272.1569217185.jpg?c=2&imbypass=on"})
+    print(card_image)
+
+
 get_card_name()
+get_card_image()
 get_card_price()
