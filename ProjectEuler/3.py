f8592610d483b4c3669c17f160929e55936a5f23
miniBIGboss <minibigboss2@gmail.com>
2020-01-08 18:05:33 +0300

    New small learning project

--- ProjectEuler/3.py
+++ ProjectEuler/3.py
@@ -4,34 +4,72 @@
 
 testNumb = 1000
 PrimeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
-AutoPrime = [2, ]
-endNumb = 600851475143
+AutoPrime = []
+first_numb = 600851475143
 
 
-for i in range(0, 13195):
-    if i % 2 != 0 and i != 1:
-        AutoPrime.append(i)
+def look_for_prime(x, y):
 
-for i in AutoPrime:
-    if i % 3 == 0 and i != 3:
-        AutoPrime.remove(i)
+    while True:
+        if x % y == 0 and y != 1:
+            AutoPrime.append(y)
+            y += -1
+            print(y)
+        elif x % y != 0 and y != 1:
+            y += -1
+            print(y)
+        else:
+            print(AutoPrime)
+            break
 
-for i in AutoPrime:
-    if i % 5 == 0 and i != 5:
-        AutoPrime.remove(i)
+    for x in AutoPrime:
+        if x % 5 == 0:
+            AutoPrime.remove(x)
 
-for i in AutoPrime:
-    floatTest = 13195 / i
-    if type(floatTest) is float:
-        AutoPrime.remove(i)
+    for x in AutoPrime:
+        if x % 3 == 0:
+            AutoPrime.remove(x)
+
+    for x in AutoPrime:
+        if x % 2 == 0:
+            AutoPrime.remove(x)
 
 
-print(AutoPrime[-1])
+def func5(x):
+    if x % 5 == 0:
+        return 0
+    else:
+        return 1
 
-def is_prime(i):
+
+def func7(x):
+    if x % 7 == 0:
+        return 0
+    else:
+        return 1
+
+
+def func13(x):
+    if x % 13 == 0:
+        return 0
+    else:
+        return 1
+
+
+def is_prime(u):
     for x in PrimeNumbers:
-        if i % x == 0:
+        if u % x == 0:
             print(x)
 
 
+look_for_prime(600851475143, 600851475143)
 print(AutoPrime)
+b = filter(func5, AutoPrime)
+b = list(b)
+print(b)
+b = filter(func7, b)
+b = list(b)
+print(b)
+b = filter(func13, b)
+b = list(b)
+print(b)
