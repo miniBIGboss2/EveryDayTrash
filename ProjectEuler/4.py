37288fa7ab83802a0cf5b660cd912b357af16e4d
miniBIGboss <minibigboss2@gmail.com>
2020-01-13 16:35:31 +0300

    New small learning project

--- ProjectEuler/4.py
+++ ProjectEuler/4.py
@@ -2,3 +2,24 @@
 The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 Find the largest palindrome made from the product of two 3-digit numbers."""
 
+main_list = []
+
+
+def make_list():
+    for x in reversed(range(100, 1000)):
+        for y in reversed(range(100, 1000)):
+            n = x * y
+            temp = n
+            rev = 0
+            while n > 0:
+                dig = n % 10
+                rev = rev * 10 + dig
+                n = n // 10
+            if temp == rev:
+                main_list.append(temp)
+                break
+
+
+make_list()
+main_list.sort()
+print(main_list[-1])
