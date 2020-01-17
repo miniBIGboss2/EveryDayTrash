78a432b40d004e91c658bd2697ea73a8651476ce
miniBIGboss <minibigboss2@gmail.com>
2020-01-17 20:44:38 +0300

    New small learning project

--- ProjectEuler/6.py
+++ ProjectEuler/6.py
@@ -4,3 +4,20 @@ Hence the difference between the sum of the squares of the first ten natural num
 and the square of the sum is 3025−385=2640.
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
+
+import time
+
+
+def both():
+    power_num = 0
+    sum_num = 0
+    for i in range(1, 101):
+        power_num += i ** 2
+        sum_num += i
+    sum_num = sum_num ** 2
+    print(sum_num - power_num)
+
+
+start_time = time.time()
+both()
+print(time.time() - start_time)
