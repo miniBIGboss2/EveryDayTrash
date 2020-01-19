dced0afe79083564d84067c49c751179fcf6e7e9
miniBIGboss <minibigboss2@gmail.com>
2020-01-19 12:30:41 +0300

    New small learning project

--- ProjectEuler/7.py
+++ ProjectEuler/7.py
@@ -1,26 +1,28 @@
 """By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?"""
 
+# todo 405
+
 import time
 
 
 def prime():
-    number_of_primes = 0
+    number_of_primes = 1
     number_to_check = 2
     number_of_dividers = 0
     while number_of_primes <= 10001:
-        for i in range(1, number_to_check):
-            if number_to_check % i == 0 and number_of_dividers != 3:
+        for i in range(1, number_to_check + 1):
+            if number_to_check % i == 0:
                 number_of_dividers += 1
-            elif number_of_dividers == 3:
-                number_of_dividers = 0
-                number_to_check += 1
-                break
-            elif i == number_to_check - 1 and number_of_dividers == 2:
-                number_to_check += 1
-                number_of_primes += 1
-        print(number_to_check)
-        print(number_of_primes)
+                if number_of_dividers == 2 and i != number_to_check:
+                    number_of_dividers = 0
+                    number_to_check += 1
+                elif number_of_dividers == 2 and i == number_to_check:
+                    number_of_dividers = 0
+                    print(number_to_check)
+                    number_to_check += 1
+                    print(number_of_primes)
+                    number_of_primes += 1
 
 
 start_time = time.time()
