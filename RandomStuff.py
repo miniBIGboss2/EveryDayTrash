8949dbc6ecf6177f7a9b5ae74fe7dd859d183a5f
miniBIGboss <minibigboss2@gmail.com>
2020-01-31 01:31:06 +0300

    New small learning project

---
+++ RandomStuff.py
@@ -0,0 +1,14 @@
+import math
+
+
+def issimple(a):
+    r = math.ceil(math.sqrt(a))
+    lst = []
+    for i in range(3 , r):
+        if a % i == 0:
+            if issimple(i) == []:
+                lst.append(i)
+    return lst
+
+
+print(issimple(600851475143))
