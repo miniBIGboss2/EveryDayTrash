00ec25e60eb8fcb071ff772d8341359ea1a134b9
miniBIGboss <minibigboss2@gmail.com>
2020-02-03 18:12:01 +0300

    New small learning project

---
+++ ProjectEuler/0015.py
@@ -0,0 +1,22 @@
+"""Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+there are exactly 6 routes to the bottom right corner.
+
+
+How many such routes are there through a 20×20 grid?"""
+
+import time
+
+test_grid = [
+    [00], [0o1], [0o2],
+    [10], [11], [12],
+    [20], [21], [22]
+]
+
+
+def my_func():
+    pass
+
+
+start_time = time.time()
+my_func()
+print(time.time() - start_time)
